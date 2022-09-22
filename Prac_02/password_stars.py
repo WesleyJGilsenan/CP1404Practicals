651f88be4389205cd1c487ba5bb9bbec964f86f3
Hotro <hotrodwes@gmail.com>
2022-09-22 13:39:51 +1000

    Refactor password check program to use functions

---
+++ Prac_02/password_stars.py
@@ -0,0 +1,19 @@
+def main():
+    password = get_password()
+    generate_password(password)
+
+
+def get_password():
+    user_password = input("Enter Password: ")
+    minimum_password_length = 3
+    if len(user_password) < minimum_password_length:
+        print("Not long enough")
+        user_password = input("Enter Password: ")
+    return user_password
+
+
+def generate_password(password):
+    print('*' * len(password))
+
+
+main()
