77d9eaa310e5cc335b192f12a63cc2ad6b779e5d
Hotro <hotrodwes@gmail.com>
2022-09-29 18:49:47 +1000

    Modified code to tell user what day it is Added Name to randoms.py

--- Prac_03/capitalist_conrad.py
+++ Prac_03/capitalist_conrad.py
@@ -1,5 +1,5 @@
-"""
-CP1404/CP5632 - Practical
+""" CP1404: Prac 3 Randoms | Wesley Gilsenan
+
 Capitalist Conrad wants a stock price simulator for a volatile stock.
 The price starts off at $10.00, and, at the end of every day there is
 a 50% chance it increases by 0 to 10%, and
@@ -8,7 +8,7 @@ If the price rises above $1000, or falls below $0.01, the program should end.
 The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
 """
 import random
-
+DAYS = 1
 MAX_INCREASE = 0.1  # 10%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
@@ -16,7 +16,7 @@ MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 
 price = INITIAL_PRICE
-print("${:,.2f}".format(price))
+print("Starting price: ${:,.2f}".format(price))
 
 while price >= MIN_PRICE and price <= MAX_PRICE:
     price_change = 0
@@ -32,4 +32,5 @@ while price >= MIN_PRICE and price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print("${:,.2f}".format(price))
+    print("On day", DAYS, "price is: ${:,.2f}".format(price))
+    DAYS = DAYS + 1
--- Prac_03/randoms.py
+++ Prac_03/randoms.py
@@ -1,3 +1,4 @@
+"""CP1404: Prac 3 Randoms | Wesley Gilsenan"""
 import random
 
 print(random.randint(1, 100))
