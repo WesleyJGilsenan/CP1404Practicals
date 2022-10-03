0aa0f2f19f50d3f01d3698d4359fa9485b7efe6c
Hotro <hotrodwes@gmail.com>
2022-10-03 14:58:36 +1000

    create file and completed part 2

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -9,3 +9,11 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {(sum(numbers))/5}")
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+user_input = input("Please enter your username: ")
+if user_input in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
