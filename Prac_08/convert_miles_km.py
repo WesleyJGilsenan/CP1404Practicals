fec4c76790e0733fbbfe92a5e48d14cea980dd93
Hotro <hotrodwes@gmail.com>
2022-11-16 13:31:35 +1000

    Begin stage one of Miles to Kilometres

--- Prac_08/convert_miles_km.py
+++ Prac_08/convert_miles_km.py
@@ -7,14 +7,24 @@ Wesley Gilsenan
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.60934
 
 
 class MilesToKmApp(App):
+    km_output = StringProperty()
 
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def label_calculation(self, text):
+        miles = text
+
+    #def handle_update(self, miles):
+        #self.km_output = miles * MILES_TO_KM
 
 
 MilesToKmApp().run()
