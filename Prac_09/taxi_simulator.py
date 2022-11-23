290851428778b035b00efb7da9a103b248465612
Hotro <hotrodwes@gmail.com>
2022-11-23 12:33:15 +1000

    Complete Taxi Simulator program

--- Prac_09/taxi_simulator.py
+++ Prac_09/taxi_simulator.py
@@ -13,28 +13,39 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
+    user_taxi = None
+    total_cost = 0
     print(MENU)
     user_selection = input(">>> ").lower()
     while user_selection != 'q':
         if user_selection == 'c':
             print("Taxis avaliable:")
             taxi_selection(taxis)
-            user_taxi = int(input("Choose taxi: "))
+            user_choice = int(input("Choose taxi: "))
             try:
-                current_taxi = taxis[user_taxi]
+                user_taxi = taxis[user_choice]
             except IndexError:
                 print("Invalid taxi choice")
             print(MENU)
             user_selection = input(">>>").lower()
         elif user_selection == 'd':
-            print("drive")
+            if user_taxi:
+                user_taxi.start_fare()
+                distance_to_drive = float(input("Drive how far? "))
+                user_taxi.drive(distance_to_drive)
+                trip_cost = user_taxi.get_fare()
+                print(f" {user_taxi.name} trip cost you ${trip_cost}")
+                total_cost += trip_cost
+                print(f"Bill to date: ${total_cost}")
             print(MENU)
             user_selection = input(">>>").lower()
         else:
             print("Invalid option")
             print(MENU)
             user_selection = input(">>>").lower()
+    print(f"Total trip cost: ${total_cost}")
     print("Taxis are now:")
+    taxi_selection(taxis)
 
 
 def taxi_selection(taxis):
