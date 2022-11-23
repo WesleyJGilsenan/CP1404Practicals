43197787ebcc940ae4f4d0ada700572bb3b376b1
Hotro <hotrodwes@gmail.com>
2022-11-23 12:04:44 +1000

    Create Taxi Selection function

--- Prac_09/taxi_simulator.py
+++ Prac_09/taxi_simulator.py
@@ -9,6 +9,7 @@ from Prac_09.silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
 
+
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
@@ -17,6 +18,12 @@ def main():
     while user_selection != 'q':
         if user_selection == 'c':
             print("Taxis avaliable:")
+            taxi_selection(taxis)
+            user_taxi = int(input("Choose taxi: "))
+            try:
+                current_taxi = taxis[user_taxi]
+            except IndexError:
+                print("Invalid taxi choice")
             print(MENU)
             user_selection = input(">>>").lower()
         elif user_selection == 'd':
@@ -28,6 +35,11 @@ def main():
             print(MENU)
             user_selection = input(">>>").lower()
     print("Taxis are now:")
+
+
+def taxi_selection(taxis):
+    for counter, taxi in enumerate(taxis):
+        print(f"{counter} - {taxi}")
 
 
 main()
