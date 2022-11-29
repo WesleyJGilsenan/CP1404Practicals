98056f5b0ae94ba6c03c35f6391cc3e254702f32
Hotro <hotrodwes@gmail.com>
2022-11-29 23:41:56 +1000

    Complete wiki program

--- Prac_10/wiki.py
+++ Prac_10/wiki.py
@@ -5,6 +5,12 @@ import wikipedia
 
 user_input = input("Please enter your search: ")
 while user_input != '':
-    print(wikipedia.search(user_input))
-    print(wikipedia.summary(user_input))
+    try:
+        print(wikipedia.search(user_input))
+        current_page = wikipedia.page(user_input, auto_suggest=False)
+        print(current_page.title)
+        print(wikipedia.summary(user_input))
+        print(current_page.url)
+    except wikipedia.exceptions.DisambiguationError:
+        print("error")
     user_input = input("Please enter your search: ")
